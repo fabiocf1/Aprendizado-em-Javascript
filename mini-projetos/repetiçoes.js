44642b267083ef8d30309320b349afcc173c26bb
Fabio Coutinho <fabio_cky@hotmail.com>
2021-11-17 15:01:27 -0300

    Aprendendo repetiçoes

---
+++ mini-projetos/repetiçoes.js
@@ -0,0 +1,21 @@
+/* 
+var c = 1
+do {
+    console.log("tudo bem?")
+    c++
+} while (c <= 6)
+
+ ESTE É O EXEMPLO COM TESTE LOGICO NO FIM
+ */
+
+
+
+/*
+var c =1
+while (c <= 6) {
+    console.log("tudo bem?")
+    c++
+}
+
+ESTE É O EXEMPLO COM TESTE LOGICO NO INICIO
+*/
