32818c25387493cc2bf3fa2de1b6db720e346101
Fabio Coutinho <fabio_cky@hotmail.com>
2021-11-23 15:47:05 -0300

    Começando exercicios sozinho com repetições

---
+++ mini-projetos/exercicio-repeticoes/script.js
@@ -0,0 +1,4 @@
+function contar()
+var inicio
+var fim
+var passo
--- mini-projetos/repetiçoes.js
+++ mini-projetos/repetiçoes.js
@@ -1,21 +1,21 @@
-/* 
-var c = 1
-do {
-    console.log("tudo bem?")
+    var c = 1
+    do {
+    console.log(`Passo ${c}`)
     c++
-} while (c <= 6)
+    } while (c <= 6)
+ /* ESTE É O EXEMPLO COM TESTE LOGICO NO FIM */
 
- ESTE É O EXEMPLO COM TESTE LOGICO NO FIM
- */
-
-
-
-/*
-var c =1
-while (c <= 6) {
-    console.log("tudo bem?")
+ 
+    var c = 1
+    while (c <= 10) {
+    console.log(`Passo ${c}`)
     c++
-}
+    }
+/* ESTE É O EXEMPLO COM TESTE LOGICO NO INICIO */
 
-ESTE É O EXEMPLO COM TESTE LOGICO NO INICIO
-*/
+
+    var c = 1
+    for(c=1; c<=10; c++){
+    console.log(`Passo ${c}`)
+    }
+/* ESTE É O EXEMPLO USANDO O "FOR", É O MAIS RECOMENDADO */
