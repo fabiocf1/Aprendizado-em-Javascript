874273b00f946d85918a51eff94ec750a1f4a2e3
Fabio Coutinho <fabio_cky@hotmail.com>
2022-01-11 12:03:09 -0300

    Novos aprendizados

---
+++ mini-projetos/APRENDIZADO JS/função-retorna-função.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+   a * b
+   return a * b
+ }
+
+ /* O codigo acima foi feito a partir do seguinte problema:
+
+ This code does not execute properly. Try to figure out why:
+ function multiply(a, b) {
+  a * b
+} 
+
+A solução foi que a função nao estava retornando nada, por isso adicionei o comando "Return A * B" para retornar um resultado.
+*/
